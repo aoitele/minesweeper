4520bab69de9136678f3abb57966e08acb0b4286
Takuto Eto <aoitele@gmail.com>
2018-11-14 19:28:38 +0900

    task③まで終了

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const width = 10;
 const height = 10;
+let count = 0;
 const bomCount = 10;
-var board = [];
-i=0;
+const board = [];
 
 // 0埋めの配列を作成(①)
 // for(x=0; x<height; ++x){
@@ -30,22 +30,37 @@ for(x=0; x<height; ++x){
 board.push(arr);
 };
 
-// hasBomをfalseからtrueに変更する処理
-while (i==bomCount) {
-  board[x][y].hasBom = true;
-  i += 1;
-  console.log("作動しています");
+// 配列のhasBom(true)をカウントする
+// for(var k in board){
+//   for(var i in board){
+//     count = board[k][k].hasBom;
+//     console.log(count);
+//   }
+// }
+
+// hasBomをfalse→trueに変更する
+while (count < bomCount) {
+// ランダムに１から９の乱数を生成
+  x = Math.floor(Math.random() * 10);
+  y = Math.floor(Math.random() * 10);
+  // console.log("施行"+count+"回目");
+  
+  if (board[x][y].hasBom == false){ 
+    board[x][y].hasBom = true;
+    console.log(x+"/"+y+"に爆弾が設置されました");
+    count++;
+  }
 }
 
 app.get('/', (req, res) => {
 // 爆弾のある位置を返さないように要素を削除する
-  // update_arr = board;
-  // for(x=0; x<height; ++x){
-  //   for(y=0; y<width; ++y){
-  //     delete update_arr[x][y].hasBom;
-  //   };
-  // };
-  // res.json(update_arr);
+  update_arr = board;
+  for(x=0; x<height; ++x){
+    for(y=0; y<width; ++y){
+      delete update_arr[x][y].hasBom;
+    };
+  };
+  res.json(update_arr);
 
 });
 
@@ -56,7 +71,7 @@ app.get('/board', (req, res) => {
   if(x|y != null){
   // 加算する場所と加算後の値
     // board[y][x] += 1;
-    board[x][y].hasBom = true;
+    board[x][y].opened = true;
     res.json(board);
     console.log(board);
   };
